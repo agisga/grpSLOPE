d4826a94355457f534673c8506e7f2b9f62d7ce5
agisga <alexej.go@googlemail.com>
2015-12-27 23:34:17 +0100

    adds the function grpSLOPE() and a unit test

--- R/grpslope.R
+++ R/grpslope.R
@@ -19,6 +19,10 @@
 #'
 #' @export
 proxGroupSortedL1 <- function(y, group, lambda, method = "c") {
+  # TODO: Instead of computing group.id here, it should be passed as an argument. Also, 
+  # this function should be probably be rewritten from scratch in C++, in order
+  # to remove the dependency on the package SLOPE
+
   group.unique <- unique(group)
   n.group <- length(group.unique)
   group.id <- list()
@@ -44,4 +48,187 @@ proxGroupSortedL1 <- function(y, group, lambda, method = "c") {
   }
 
   return(prox.solution)
+}
+
+#' Group SLOPE algorithm
+#'
+#' Compute the coefficient estimates of the Group SLOPE model  
+#'
+#' @export
+grpSLOPE <- function(X, Dinv, b, group, lambda, opts=list())
+{
+  # TODO: rewrite this function from scratch, where the main loop should be written in C++
+  # TODO: check whether all groups have length 1, then use SLOPE
+
+  # Copyright 2013, M. Bogdan, E. van den Berg, W. Su, and E.J. Candes
+  # Copyright 2015, Alexej Gossmann
+
+  # -------------------------------------------------------------
+  # Start times
+  # -------------------------------------------------------------
+  t0 <- proc.time()[3]
+  
+  # -------------------------------------------------------------
+  # Define function for retrieving option fields with defaults
+  # -------------------------------------------------------------
+  getDefaultField <- function(opts,name,default)
+  {  
+    if (!is.null(opts[[name]])) {
+      return(opts[[name]])
+    } else {
+      return(default)
+    }
+  }
+  
+  # -------------------------------------------------------------
+  # Parse parameters
+  # -------------------------------------------------------------
+  iterations <- getDefaultField(opts,"iterations", 10000)
+  verbosity  <- getDefaultField(opts,"verbosity" , 1)
+  gradIter   <- getDefaultField(opts,"gradIter"  , 20)
+  tolerance  <- getDefaultField(opts,"tolerance" , 1e-6)
+  xInit      <- getDefaultField(opts,"xInit"     , vector())
+  
+  # -------------------------------------------------------------
+  # Ensure that lambda is non-increasing
+  # -------------------------------------------------------------
+  n.lambda <- length(lambda)
+  if ((n.lambda > 1) && any(lambda[2:n.lambda] > lambda[1:n.lambda-1])) {
+    stop("Lambda must be non-increasing.")
+  }
+  
+  # -------------------------------------------------------------
+  # Initialize
+  # -------------------------------------------------------------
+
+  A <- X %*% Dinv
+  n <- ncol(A)
+  
+  # Get initial lower bound on the Lipschitz constant
+  x <- matrix(rnorm(n),c(n,1))
+  x <- x / norm(x, "f")
+  x <- t(A) %*% (A %*% x)
+  L <- norm(x, "f")
+  
+  # Set constants
+  STATUS_RUNNING    <- 0
+  STATUS_OPTIMAL    <- 1
+  STATUS_ITERATIONS <- 2
+  STATUS_MSG        <- c('Optimal','Iteration limit reached')
+  
+  # Initialize parameters and iterates
+  if (length(xInit) == 0) xInit <- matrix(0,n,1)
+  tt      <- 1
+  eta     <- 2
+  lambda  <- as.matrix(lambda)
+  b       <- as.matrix(b)
+  x       <- xInit
+  y       <- x
+  Ax      <- A %*% x
+  fPrev   <- Inf
+  iter    <- 0
+  status  <- STATUS_RUNNING
+  Aprods  <- 2
+  ATprods <- 1
+  relgap  <- Inf
+  
+  if (verbosity > 0) {
+    printf <- function(...) invisible(cat(sprintf(...)))
+    printf('%5s  %9s  %9s\n','Iter','||r||_2','Rel. gap')
+  }
+  
+  # -------------------------------------------------------------
+  # Main loop
+  # -------------------------------------------------------------
+  while (TRUE)
+  {    
+    # Compute the gradient at f(y) (Includes first iterations)
+    if ((iter %% gradIter) == 0) {
+      r <- (A %*% y) - b
+      g <- t(A) %*% r
+      f <- as.double(crossprod(r)) / 2
+    } else {
+      r <- (Ax + ((ttPrev - 1) / tt) * (Ax - AxPrev)) - b
+      g <- t(A) %*% r
+      f <- as.double(crossprod(r)) / 2
+    }
+    
+    # Increment iteration count
+    iter <- iter + 1
+    
+    # Stopping criteria
+    if ((status == 0) && (iter >= iterations)) {
+      status <- STATUS_ITERATIONS
+    }
+
+    if (status != 0) {
+      if (verbosity > 0) {
+        printf('Exiting with status %d -- %s\n', status, STATUS_MSG[[status]])
+      }
+      break
+    }
+    
+    # Keep copies of previous values
+    AxPrev <- Ax
+    xPrev  <- x
+    yPrev  <- y
+    fPrev  <- f
+    ttPrev <- tt
+    
+    # Lipschitz search
+    while (TRUE) {
+      # Compute prox mapping
+      x <- proxGroupSortedL1(y = y - (1/L)*g, group = group, lambda = lambda/L)
+      d <- x - y
+    
+      Ax <- A %*% x
+      r  <- Ax-b
+      f  <- as.double(crossprod(r))/2
+      q  <- fPrev + as.double(crossprod(d,g)) + (L/2)*as.double(crossprod(d))
+                              
+      Aprods <- Aprods + 1
+                              
+      if (q >= f*(1-1e-12))
+        break
+      else
+        L <- L * eta
+    }
+    
+    # Update
+    tt <- (1 + sqrt(1 + 4*tt^2)) / 2
+    y  <- x + ((ttPrev - 1) / tt) * (x - xPrev)
+
+    # Compute 'relative gap'
+    if (sqrt(sum(yPrev^2))==0) {
+      relgap <- sqrt(sum((y-yPrev)^2))
+    } else {
+      relgap <- sqrt(sum((y-yPrev)^2)) / sqrt(sum(yPrev^2))
+    }
+
+    # Format string
+    if (verbosity > 0) str <- sprintf('   %9.2e', relgap)
+     
+    # Check relative gap
+    if (relgap < tolerance) status <- STATUS_OPTIMAL
+    
+    if ( verbosity > 0 ) {
+      printf('%5d  %9.2e%s\n', iter,f,str)
+    }
+  } # While (TRUE)
+  
+  
+  # Information structure
+  solution           <- list()
+  solution$runtime   <- proc.time()[3] - t0
+  solution$Aprods    <- Aprods + ceiling(iter / gradIter)
+  solution$ATprods   <- ATprods + iter
+  solution$status    <- status
+  solution$x         <- Dinv %*% y
+  solution$L         <- L
+  solution$iter      <- iter
+
+  # Define an S3 class
+  class(solution) <- "grpSLOPE"
+  
+  return(solution)
 }
--- tests/testthat/test_proximal_gradient_method.R
+++ tests/testthat/test_proximal_gradient_method.R
@@ -1,6 +1,6 @@
 library(grpSLOPE)
 
-context("Proximal gradient method")
+context("proxGroupSortedL1()")
 
 test_that("the prox is evaluated correctly if the groups are consequtive blocks", {
   x   <- proxGroupSortedL1(y = 1:10, group = c(1,1,1,2,2,2,3,3,3,4), lambda = 10:1)
@@ -16,5 +16,22 @@ test_that("the prox is evaluated correctly if the groups are not consequtive blo
            1.974292890, 2.256334731, 2.538376572, 1.0000000001)
   ord <- sample(y, 10)
   x <- proxGroupSortedL1(y = y[ord], group = grp[ord], lambda = 10:1)
-  expect_equal(x, sol[ord])
+  expect_equal(x, sol[ord], tolerance=1e-6)
+})
+
+
+
+context("grpSLOPE()")
+
+test_that("the Group SLOPE solution is computed correctly", {
+  set.seed(1)
+  A    <- matrix(runif(100, 0, 1), 10, 10)
+  grp  <- c(0, 0, 1, 1, 2, 2, 2, 2, 2, 3)
+  Dinv <- diag(c(0.5, 0.5, 0.5, 0.5, 0.2, 0.2, 0.2, 0.2, 0.2, 1))
+  x    <- c(0, 0, 5, 1, 0, 0, 0, 1, 0, 3)
+  y    <- A %*% x
+  result <- grpSLOPE(X=A, Dinv=Dinv, b=y, group=grp, lambda=0.1*(10:1),
+                   list("tolerance" = 1e-12, "verbosity" = 0))
+  solution  <- matrix(c(0,0,3.856002988,2.080742942,0,0,0,0,0,3.512828045), c(10,1))
+  expect_equal(result$x, solution, tolerance=1e-6)
 })
